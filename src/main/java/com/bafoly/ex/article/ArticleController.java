70d0e6fa280fd62d7edb5d35cdf18fe6e3ce1651
Basar Buyukkahraman <basarbk@yahoo.com>
2020-09-24 23:46:19 +0300

    same exception handler for article

--- src/main/java/com/bafoly/ex/article/ArticleController.java
+++ src/main/java/com/bafoly/ex/article/ArticleController.java
@@ -1,5 +1,34 @@
 package com.bafoly.ex.article;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.bafoly.ex.error.ApiError;
+import com.bafoly.ex.error.NotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ArticleController {  
+
+  @Autowired
+  ArticleService articleService;
+
+  @GetMapping("/articles/{id}")
+  public Article getArticle(@PathVariable long id){
+    return this.articleService.getArticleById(id);
+  }
   
+
+  @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public ApiError handleNotFoundException(NotFoundException ex, HttpServletRequest request){
+    ApiError error = new ApiError(404, ex.getMessage(), request.getServletPath());
+    return error;
+  }
 }
--- src/main/java/com/bafoly/ex/article/ArticleService.java
+++ src/main/java/com/bafoly/ex/article/ArticleService.java
@@ -1,5 +1,18 @@
 package com.bafoly.ex.article;
 
+import com.bafoly.ex.error.NotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ArticleService {
+
+  @Autowired
+  ArticleRepository articleRepository;
+
+	public Article getArticleById(long id) {
+		return this.articleRepository.findById(id).orElseThrow(() -> new NotFoundException("Article does not exist!"));
+	}
   
 }
